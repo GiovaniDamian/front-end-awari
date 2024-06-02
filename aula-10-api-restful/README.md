c8d37fde4cad64f051033dfe84ff57ea4d300038
Giovani Damian <gedamian@gmail.com>
2024-06-02 12:26:29 -0300

    aula-10-react-api

---
+++ aula-10-api-restful/README.md
@@ -0,0 +1,12 @@
+# Prática: Lista de Pokémons
+
+Nesta prática, as tarefas foram:
+
+- fazer requisições para API PokéAPI (https://pokeapi.co/):
+   * primeiro, obter a lista inicial de Pokemons no endpoint https://pokeapi.co/api/v2/pokemon?limit=20;
+   * em seguida, obter os detalhes de cada um dos Pokémons obtidos na requisição anterior;
+- deve-se armazenar os detalhes obtidos em useState;
+- por fim, exibir nome e imagem dos Pokémons na página.
+
+## Resultado
+<img height="260em" src="https://github.com/GiovaniDamian/front-end-awari/assets/60575219/66b43dec-bb74-434f-8e3a-ed067c3b43fc"/>
---
+++ aula-10-api-restful/pokemon/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import List from './components/List'
+
+function App() {
+    const [items, setItems] = useState([]);
+
+    const loadDetails = (items) => {
+        const promises = items.map((item) => {
+            return fetch(item.url).then((response) => response.json())
+        });
+        Promise.all(promises)
+            .then((data) => {
+                setItems(data);
+            });
+    }
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const { results } = data;
+                loadDetails(results);
+            })
+            .catch(() => {
+                console.error('Error');
+            });
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <img alt="Pokemon API" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
+            </div>
+            <List items={items}  />
+        </div>
+    );
+}
+
+export default App;
