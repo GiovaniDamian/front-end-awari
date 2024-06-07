3489228e01bbcae18ec1a8684e67dc1ca88f823a
Giovani Damian <gedamian@gmail.com>
2024-06-07 16:48:17 -0300

    aula-15-optimization-SEO

---
+++ aula-15-Optimization-SEO/README.md
@@ -0,0 +1,12 @@
+# Prática: Documentação com Storybook
+
+Nesta prática, o projeto “Lista de Pokémons” será incrementado com React Storybook. As tarefas foram:
+
+* instalar o Storybook no projeto “Lista de Pokémons”, criado nas aulas anteriores;
+* criar uma página no Storybook que deve conter o conteúdo presente no arquivo README.md;
+* criar um Stories para cada um dos componentes da aplicação (Heart, List e ListItem).
+
+## Resultado
+
+<img height="260em" src="https://github.com/GiovaniDamian/front-end-awari/assets/60575219/c7b7186f-dfa3-42d6-90f8-17a40df6f4c7"/>
+
---
+++ aula-15-Optimization-SEO/pokemon/src/components/ListItem.test.jsx
@@ -0,0 +1,69 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import ListItem from '../components/ListItem';
+import { actions } from '../store/favorites/favorites.slice';
+import userEvent from '@testing-library/user-event';
+
+const mockedItem = {
+    id: 1,
+    name: 'bulbasaur',
+    sprites: {
+        other: {
+            'official-artwork': {
+                front_default: ''
+            }
+        }
+    }
+};
+
+const mockStore = configureStore([]);
+
+describe('ListItem Component', () => {
+    let store;
+
+    beforeEach(() => {
+        store = mockStore({
+            favorites: []
+        });
+
+        store.dispatch = jest.fn();
+    });
+
+    it('should render correctly', () => {
+        render(
+            <Provider store={store}>
+                <ListItem item={mockedItem} />
+            </Provider>
+        );
+        expect(screen.getByTestId('list-item-1')).toBeInTheDocument();
+    });
+
+    it('should dispatch add action when heart is clicked and item is not favorite', async () => {
+        render(
+            <Provider store={store}>
+                <ListItem item={mockedItem} />
+            </Provider>
+        );
+
+        await userEvent.click(screen.getByTestId('heart-1'));
+        expect(store.dispatch).toHaveBeenCalledWith(actions.add(mockedItem));
+    });
+
+    it('should dispatch remove action when heart is clicked and item is favorite', async () => {
+        store = mockStore({
+            favorites: [1]
+        });
+
+        store.dispatch = jest.fn();
+
+        render(
+            <Provider store={store}>
+                <ListItem item={mockedItem} />
+            </Provider>
+        );
+
+        await userEvent.click(screen.getByTestId('heart-1'));
+        expect(store.dispatch).toHaveBeenCalledWith(actions.remove(mockedItem));
+    });
+});
